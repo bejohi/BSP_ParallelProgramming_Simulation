2c8d8a1dcf8bfb5f2e99020964d1f0b72fa2975a
Jonas Hielscher <benedikt.hielscher@st.ovgu.de>
2018-04-19 11:10:05 +0200

    ADDED: A MAKEFILE.

---
+++ Makefile
@@ -0,0 +1,11 @@
+all: build
+	./bin/task1
+
+build: prep
+	gcc -std=c99 -Wall -O3 -o bin/task1 src/task1.c libmcbsp1.2.0.a -pthread -lrt -lm
+
+prep: clean
+	mkdir bin/
+
+clean:
+	rm -rf bin/
--- src/task1.c
+++ src/task1.c
@@ -6,11 +6,44 @@
  */ 
 static unsigned int numberOfProcessors;
 
+static long globalN;
+
+void initMatrix(double** matrix){
+    matrix = (double**) malloc(sizeof(double*) * globalN);
+    if(!matrix){
+        bsp_abort("Not enough memory for allocating the matrix available\n");
+    }
+    for(double i = 0; i < globalN; i++){
+        matrix[i] = (double*) malloc(sizeof(double) * globalN);
+        if(!matrix[i]){
+            bsp_abort("Not enough memory for allocating the matrix available\n");
+        }
+    }
+}
+
+
 /**
  * The BSP Main method to run on multiple machines.
  */
 void bspEntrance(){
+
     bsp_begin(numberOfProcessors);
+    long p= bsp_nprocs();
+    long s= bsp_pid();
+
+    bsp_push_reg(&n,sizeof(long));
+    bsp_sync();
+
+    bsp_get(0,&globalN,0,&globalN,sizeof(long));
+    bsp_sync();
+    bsp_pop_reg(&globalN);
+
+    
+
+    double** matrix;
+    initMatrix(matrix);
+
+    bsp_end();
 }
 
 int main(int argc, char **argv){
